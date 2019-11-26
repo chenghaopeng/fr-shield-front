3e0f9516e207e3558fb01c5b7611ccf84fdc084e
chenghaopeng <i@chper.cn>
2019-11-26 23:46:47 +0800

    所有界面推倒重来

--- src/router/Main/index.js
+++ src/router/Main/index.js
@@ -1,69 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./index.module.less";
 
-import MdSearch from "react-ionicons/lib/MdSearch";
-import { withRouter } from "react-router-dom"
-
-import { message, Popover } from "antd";
-
-import WithHeader from "../../component/WithHeader";
-
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {searchMode: 0};
-  }
-
-  getStateName = () => {
-    if (this.state.searchMode === 0) return "财务分析";
-    if (this.state.searchMode === 1) return "数据分析";
-  }
-
-  toInput = (id) => {
-    this.setState({...this.state, searchMode: id});
-    document.getElementById("stock").focus();
-  }
-
-  toSearch = () => {
-    var stock = document.getElementById("stock").value.trim();
-    if (stock === "") {
-      message.error("请输入股票代码或名称！");
-    }
-    else {
-      if (this.state.searchMode === 0) this.props.history.push("/analysis/" + stock);
-      else if (this.state.searchMode === 1) this.props.history.push("/information/" + stock);
-    }
-  }
-
-  toForum = () => {
-    message.loading("正在加载用户论坛...", Math.random() * .8, () => {this.props.history.push("/forum");});
-  }
-  
-  toPersonal = () => {
-    this.props.history.push("/personal");
-  }
-
+class Main extends React.Component {
   render() {
-  const content = (<p style={{fontSize: "3vh"}}>{this.getStateName()}：请输入股票代码或名称</p>);
     return (
-      <div className={styles.whole}>
-        <div className={styles.searchBox}>
-          <Popover content={content} trigger="focus">
-            <input id="stock" name="stock" type="text" className={styles.stock} autoComplete="off" placeholder={this.getStateName()}/>
-          </Popover>
-          <button className={styles.search} onClick={this.toSearch}><MdSearch fontSize="6vh" color="rgb(89, 77, 73)"/></button>
-        </div>
-        {/* <div className={styles.iconBox}>
-        </div> */}
-        <div className={styles.toolBox}>
-          <button className={styles.cmdButton} onClick={this.toInput.bind(this, 0)}>财务分析</button>
-          <button className={styles.cmdButton} onClick={this.toInput.bind(this, 1)}>数据分析</button>
-          <button className={styles.cmdButton} onClick={this.toForum}>用户论坛</button>
-          <button className={styles.cmdButton} onClick={this.toPersonal}>个人中心</button>
-        </div>
+      <div>
+        船新的开始
       </div>
     );
   }
 }
 
-export default WithHeader(withRouter(Main));
+export default Main;
---
+++ src/router/RouteConfig/BasicRoute.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Main from "../Main";
+
+export default [
+	<Route component={ Main } exact path='/'/>,
+];
