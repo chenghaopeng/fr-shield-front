e1543d08be77cb14b8a1a6927c7b122f2b1d49b2
chenghaopeng <i@chper.cn>
2019-11-27 23:12:33 +0800

    综合分析框架

--- src/router/Analysis/index.js
+++ src/router/Analysis/index.js
@@ -2,15 +2,64 @@ import React from "react";
 import styles from "./index.module.less";
 import Frame from "../../components/Frame";
 import { withRouter } from "react-router"
+import FlatButton from "../../components/FlatButton/FlatButton";
+import { Row, Col } from "antd";
 
 class Analysis extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {stockname: this.props.match.params.stock, stockcode: "", view: 1, status: [false, false, false], datas: [{}, {}, {}]};
+    this.refresh(this.props.match.params.stock);
+  }
+  refresh = (stockname) => {
+    this.setState({stockname: stockname, stockcode: "", view: 1, status: [false, false, false], datas: [{}, {}, {}]});
+  }
+  toView = view => {
+    this.setState({...this.state, view: view});
+  }
   render() {
     const component = (
       <div className={styles.whole}>
-        
+        <div className={`${styles.defaultBox} ${styles.defaultRow}`}>
+          {this.state.stockname}
+        </div>
+        <div className={styles.tabs}>
+          <FlatButton text="股票资料" onClick={this.toView.bind(this, 1)} />
+          <FlatButton text="数据分析" onClick={this.toView.bind(this, 2)} />
+          <FlatButton text="舆情分析" onClick={this.toView.bind(this, 3)} />
+          <FlatButton text="财务分析" onClick={this.toView.bind(this, 4)} />
+        </div>
+        { this.state.view === 1 ?
+          <Row className={styles.defaultRow}>
+            <Col span={24} className={styles.defaultBox}>
+              这是股票资料，以后再说
+            </Col>
+          </Row>
+        : ""}
+        { this.state.view === 2 ?
+          <Row className={styles.defaultRow}>
+            <Col span={24} className={styles.defaultBox}>
+              这是数据分析
+            </Col>
+          </Row>
+        : ""}
+        { this.state.view === 3 ?
+          <Row className={styles.defaultRow}>
+            <Col span={24} className={styles.defaultBox}>
+              这是舆情分析
+            </Col>
+          </Row>
+        : ""}
+        { this.state.view === 4 ?
+          <Row className={styles.defaultRow}>
+            <Col span={24} className={styles.defaultBox}>
+              这是财务分析
+            </Col>
+          </Row>
+        : ""}
       </div>
     );
-    return <Frame node={component} title={"综合分析 " + this.state.stockname} />
+    return <Frame node={component} title={"综合分析[" + this.state.stockname + "]"} that={this} />
   }
 }
 
