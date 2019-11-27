8680ae00166360bd7bb0c127efb51cb2e2d743af
chenghaopeng <i@chper.cn>
2019-11-27 23:16:38 +0800

    根据登录状态显示导航栏

--- src/components/Frame/index.js
+++ src/components/Frame/index.js
@@ -8,7 +8,7 @@ import { logout } from "../../services/apiHTTP";
 class Frame extends React.Component {
   constructor(props) {
     super(props);
-    if (!window.sessionStorage.token && this.props.title !== "个人中心") this.props.history.push("/user");
+    if (!window.sessionStorage.token && this.props.title !== "个人中心" && this.props.title) this.props.history.push("/user");
   }
   handleSearch = (e) => {
     if (e.keyCode === 13) {
@@ -47,15 +47,17 @@ class Frame extends React.Component {
           </div>
           <div className={styles.navigator}>
             <FlatButton text="首 页" href="/" />
-            <FlatButton text="股票浏览" href="/stocks" />
-            <FlatButton text="用户论坛" href="/forum" />
-            <FlatButton text="个人中心" href="/user" />
+            {window.sessionStorage.token ? <FlatButton text="股票浏览" href="/stocks" /> : ""}
+            {window.sessionStorage.token ? <FlatButton text="用户论坛" href="/forum" /> : ""}
+            <FlatButton text={window.sessionStorage.token ? "个人中心" : "登录"} href="/user" />
             {window.sessionStorage.token ? <FlatButton text="登出" onClick={this.handleLogout} /> : ""}
           </div>
         </div>
-        <div className={styles.searchBox}>
-          <Input className={`${styles.defaultShadowBox} ${styles.searchText}`} placeholder="搜索股票名称或代码.." onKeyDown={this.handleSearch} />
-        </div>
+        {window.sessionStorage.token ? 
+          <div className={styles.searchBox}>
+            <Input className={`${styles.defaultShadowBox} ${styles.searchText}`} placeholder="搜索股票名称或代码.." onKeyDown={this.handleSearch} />
+          </div>
+        : ""}
         <div className={styles.content}>
           {this.props.node}
         </div>
