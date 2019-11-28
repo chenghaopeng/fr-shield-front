5ae0c5354700e8b6f2b64f8eb08f3ead7603fa1e
chenghaopeng <i@chper.cn>
2019-11-28 23:13:44 +0800

    优化联想

--- src/utils/stocks.js
+++ src/utils/stocks.js
@@ -8,9 +8,13 @@ export const FindStock = (stock) => {
 }
 
 export const AssociateStock = (stock) => {
-  let possible = [];
+  let possible = [], other = [];
   for (let i = 1; i < Stocks.length && possible.length < 10; ++i) {
-    if (Stocks[i][0].indexOf(stock) !== -1 || Stocks[i][1].indexOf(stock) !== -1) possible.push([Stocks[i][0], Stocks[i][1]]); 
+    if (Stocks[i][0].indexOf(stock) === 0 || Stocks[i][1].indexOf(stock) === 0) possible.push([Stocks[i][0], Stocks[i][1]]); 
+    else if (Stocks[i][0].indexOf(stock) !== -1 || Stocks[i][1].indexOf(stock) !== -1) other.push([Stocks[i][0], Stocks[i][1]]); 
+  }
+  for (let i = 0; i < other.length && possible.length < 10; ++i) {
+    possible.push(other[i]);
   }
   return possible;
 }
