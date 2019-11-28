b432af1c0f27defe83d94203f526f2a90c6d6bb4
chenghaopeng <i@chper.cn>
2019-11-29 00:41:29 +0800

    添加logo

--- src/router/Main/index.js
+++ src/router/Main/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./index.module.less";
 import Frame from "../../components/Frame";
+import logo from "../../themes/logo.png";
 
 class Main extends React.Component {
   render() {
     const component = (
       <div className={styles.whole}>
-        这里是首页，需要一些展示性的东西，比如项目介绍，logo啥的
+        <div className={`${styles.defaultBox} ${styles.main}`}>
+          <img src={logo} alt="logo" />
+        </div>
       </div>
     );
     return (<Frame node={component}/>);
