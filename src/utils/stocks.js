faefbaa1808b6dae84b2f46457fb5fc61e5a131c
chenghaopeng <i@chper.cn>
2019-11-28 22:30:15 +0800

    修改联想工具

--- src/utils/stocks.js
+++ src/utils/stocks.js
@@ -8,9 +8,9 @@ export const FindStock = (stock) => {
 }
 
 export const AssociateStock = (stock) => {
-  let possiable = [];
-  for (let i = 1; i < Stocks.length && possiable.length < 10; ++i) {
-    if (Stocks[i][0].indexOf(stock) !== -1 || Stocks[i][1].indexOf(stock) !== -1) possiable.push([Stocks[i][0], Stocks[i][1]]); 
+  let possible = [];
+  for (let i = 1; i < Stocks.length && possible.length < 10; ++i) {
+    if (Stocks[i][0].indexOf(stock) !== -1 || Stocks[i][1].indexOf(stock) !== -1) possible.push([Stocks[i][0], Stocks[i][1]]); 
   }
-  return false;
+  return possible;
 }
