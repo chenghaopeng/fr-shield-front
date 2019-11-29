13714ca221a40fdde6622b59520f9b22c3a45a47
chenghaopeng <i@chper.cn>
2019-11-29 17:22:28 +0800

    修复日期显示bug

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,7 +1,7 @@
 export const DateFormat = (time) => {
   let p = time.indexOf("-");
-  time = time.substr(0, p) + "年" + time.substr(p);
+  time = time.substr(0, p) + "年" + time.substr(p + 1);
   p = time.indexOf("-");
-  time = time.substr(0, p) + "月" + time.substr(p);
+  time = time.substr(0, p) + "月" + time.substr(p + 1);
   return time + "日";
 }
