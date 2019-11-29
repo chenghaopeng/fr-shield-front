439dd2e2f0c5248aebb4ac7651b9992ab36220b2
chenghaopeng <i@chper.cn>
2019-11-29 17:04:30 +0800

    首页logo裁剪；添加标语；设置边距

--- src/router/Main/index.js
+++ src/router/Main/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import styles from "./index.module.less";
 import Frame from "../../components/Frame";
 import logo from "../../themes/logo.png";
+import slogan from "../../themes/slogan.png";
 
 class Main extends React.Component {
   render() {
     const component = (
       <div className={styles.whole}>
         <div className={`${styles.defaultBox} ${styles.main}`}>
-          <img src={logo} alt="logo" />
+          <img className={styles.logo} src={logo} alt="logo" />
+          <img className={styles.slogan} src={slogan} alt="slogan" />
         </div>
       </div>
     );
